2adc91f576f866d07adcf66f52d2a814169ea6cc
Oleksiy Kyrylenko <aleksey.kyrylenko@external.magnetimarelli.com>
2015-07-29 10:39:25 +0200

    added git reset author

---
+++ git-reset-author.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+git filter-branch --commit-filter \
+    'export GIT_AUTHOR_NAME="Oleksiy Kyrylenko";\
+    export GIT_AUTHOR_EMAIL="aleksey.kyrylenko@external.magnetimarelli.com";\
+    export GIT_COMMITTER_NAME="Oleksiy Kyrylenko";\
+    export GIT_COMMITTER_EMAIL="aleksey.kyrylenko@external.magnetimarelli.com";\
+fi;\
+git commit-tree "$@"'
